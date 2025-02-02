f011cec50bd1d00f095d70386620749a8b22acc7
Julien <Julien.lanctot@umontreal.ca>
2025-02-02 13:27:25 -0500

    The initial commit with stuff in it

---
+++ ClassRise/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <link rel="stylesheet" href="https://startr.style/style.css"></link>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <button>Big button</button>
+    </View>
+  );
+}
