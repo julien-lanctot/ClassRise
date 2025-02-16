4854e8e3cb65a269c0ef9637fd31422844567e57
Cleo Mandros <cleomandros@gmail.com>
2025-02-16 17:47:17 -0500

    container color changed

--- ClassRise/app/incorrectpage.tsx
+++ ClassRise/app/incorrectpage.tsx
@@ -12,29 +12,57 @@ export default function CorrectScreen() {
 
     return (
         <>
-        <View style={styles.container}></View>
-        <Text style={styles.correctText}> Incorrect... </Text>
+        <View style={styles.container}>
+        <Text style={styles.question}> Incorrect... </Text>
         <TouchableOpacity style={styles.button} onPress={onButtonClick}>
         <Text style={styles.buttonText}>Next Question</Text>
         </TouchableOpacity>
+        </View>
         </>
     );
 }
 
+
 const styles = StyleSheet.create({
-    container: {
-//fill out here
+    question:{
+      fontSize: 24,
+      color: "#2c786c",
+      textAlign: "center",
+      marginBottom: 30,
+      marginHorizontal: 30
+  
     },
-
-    correctText: {
-// fill out here
+  
+    input: {
+      width: "80%",
+      height: 50,
+      borderColor: "#2c786c",
+      borderWidth: 1,
+      borderRadius: 10,
+      paddingHorizontal: 15,
+      fontSize: 18,
+      textAlign: "center",
+      marginBottom: 20,
     },
-
+  
     button: {
-// fill out here
+      backgroundColor: "#2c786c",
+      paddingVertical: 12,
+      paddingHorizontal: 30,
+      borderRadius: 10,
+      alignItems: "center",
     },
-
+  
     buttonText: {
-        // fill out here
-            },
-});
+      color: "#ffffff",
+      fontSize: 18,
+      fontWeight: "bold",
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#d45353",
+    }
+  
+  });
