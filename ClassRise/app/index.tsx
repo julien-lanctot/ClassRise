80bea9f315ff5cec4cc54d9488b82088e340b32d
Emily Fetco <emilyfetco@Emilys-MacBook-Air.local>
2025-02-19 19:00:08 -0500

    better design background

--- ClassRise/app/index.tsx
+++ ClassRise/app/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity, ImageBackground } from "react-native";
 import { router } from "expo-router";
 
 export default function Index() {
@@ -9,12 +9,18 @@ export default function Index() {
   };
 
   return (
+    <ImageBackground
+    source={require("@/app/background.png")}
+    style={styles.background}
+    resizeMode="cover"
+    
+    >
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#a7d7c5",
+        backgroundColor: "transparent",
       }}
     >
       {/*<link rel="stylesheet" href="https://startr.style/style.css"></link>*/}
@@ -28,10 +34,18 @@ export default function Index() {
         <Text style={styles.buttonText}>Get Started</Text>
       </TouchableOpacity>
     </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+
   title: {
     fontSize: 23,
     fontWeight: "bold",
