5f78a79f6b5d5b7e80346df90e6818069aaa00a2
Brunelle Charles <melisandebrunelle373@gmail.com>
2025-02-23 18:26:29 -0500

    Added mascot to correct page, problem with frog being on same page as correct text

--- ClassRise/app/correctpage.tsx
+++ ClassRise/app/correctpage.tsx
@@ -1,8 +1,10 @@
 {/*changes here*/}
 
 import React from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, ImageBackground, Image, StyleSheet } from "react-native";
 import { router } from "expo-router";
+
+
 
 export default function CorrectScreen() {
 
@@ -11,58 +13,72 @@ export default function CorrectScreen() {
     };
 
     return (
-        <>
-        <View style={styles.container}>
-                <Text style={styles.question}> Correct!!! </Text>
-                <TouchableOpacity style={styles.button} onPress={onButtonClick}>
-                <Text style={styles.buttonText}>Next Question</Text>
-                </TouchableOpacity>
-                </View>
-                </>
+     <ImageBackground
+     style={styles.question} 
+
+    >    
+        <View style={styles.question}>
+        <Image source={require("@/app/mascot.png")} style={styles.question} />
+        <Text style={styles.question}> Correct... </Text>
+        <TouchableOpacity style={styles.button} onPress={onButtonClick}>
+        <Text style={styles.buttonText}>Next Question</Text>
+        </TouchableOpacity>
+        </View>
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+ background: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#a8f7cc",
-    },
-  
-    question:{
-        fontSize: 24,
-        color: "#22945f",
-        textAlign: "center",
-        marginBottom: 30,
-        marginHorizontal: 30
-    
-      },
-    
-      input: {
-        width: "80%",
-        height: 50,
-        borderColor: "#2c786c",
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingHorizontal: 15,
-        fontSize: 18,
-        textAlign: "center",
-        marginBottom: 20,
-      },
-    
-      button: {
-        backgroundColor: "#22945f",
-        paddingVertical: 12,
-        paddingHorizontal: 30,
-        borderRadius: 10,
-        alignItems: "center",
-      },
-     
-      buttonText: {
-        color: "#ffffff",
-        fontSize: 18,
-        fontWeight: "bold",
-      },
-    
+        width: "100%",
+        height: "100%",
+  },
+  question:{
+    fontSize: 24,
+    color: "##95de95",
+    textAlign: "center",
+    marginBottom: 30,
+    marginHorizontal: 30
+
+  },
+
+  input: {
+    width: "80%",
+    height: 50,
+    borderColor: "#95de95",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+
+  mascot: {
+    width: 250,
+    height: 250,
+    marginBottom: 20,
+  },
+
+  button: {
+    backgroundColor: "#95de95",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+
+  buttonText: {
+    color: "#026e02",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+ 
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#38fc38",
+}
 });
